61a9030b11bf4ebf7221ab9862f22dae385d3701
Migueloso <miguel5963@ciencias.unam.mx>
2019-10-16 15:17:36 -0500

    Modificacion de los nombres de las views

--- pumaeventos/Eventos/forms.py
+++ pumaeventos/Eventos/forms.py
@@ -1,14 +1,11 @@
 from django import forms
-from django.forms import ModelForm
-from .models import Eventos
 
-class EventoForm(forms.Form):
-    id_organizador = forms.CharField(label='id_organizador', max_length=100)
-    nombre_evento = forms.CharField(label='nombre_evento', max_length=100)
-    ubicacion = forms.CharField(label='ubicacion', max_length=100)
-    detalles_evento= forms.CharField(label='detalles', widget=forms.Textarea)
-    cupo = forms.IntegerField(label='cupo', max_length=100)
-    fecha_inicio_evento = forms.DateTimeField(default=django.utils.timezone.now,null=False)
-    fecha_fin_evento = forms.DateTimeField(default=django.utils.timezone.now,null=False)
-    hora_inicio_evento = forms.TimeField(default=django.utils.timezone.now,null=False)
-    hora_fin_evento = forms.TimeField(default=django.utils.timezone.now,null=False)
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from .models import Evento
+
+class EventoForm(forms.ModelForm):
+    class Meta:
+        model = Evento
+        fields = ['id_organizador', 'nombre_evento', 'ubicacion', 'detalles_evento',
+        'cupo', 'fecha_inicio_evento', 'fecha_fin_evento', 'hora_inicio_evento', 'hora_fin_evento']
