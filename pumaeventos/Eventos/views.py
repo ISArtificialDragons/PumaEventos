7a0e39f2879073dcc5817465b0940b6ebf3f2556
Migueloso <miguel5963@ciencias.unam.mx>
2019-10-16 17:55:48 -0500

    Se agrego editar y eliminar eventos

--- pumaeventos/Eventos/views.py
+++ pumaeventos/Eventos/views.py
@@ -14,11 +14,32 @@ def crear_evento(request):
         evento_form = EventoForm(request.POST)
         if evento_form.is_valid():
             evento_form.save()
-            return render(request, "Evento/IHCrearEvento.html")
+            return render(request, "Usuarios/IHPrincipal.html")
 
 def editar_evento(request):
-    if request.method=='POST':
-        evento_form = EventoForm(request.POST)
+    evento = get_object_or_404(Evento, pk=pk)
+    if request.method == "POST":
+        evento_form = EventoForm(request.POST, instance=evento)
         if evento_form.is_valid():
-            evento_form.save()
-            return render(request, "Evento/IHEditarEvento.html")
+            evento = evento_form.save(commit=False)
+            evento.id_organizador = request.user
+            evento.nombre_evento = request.user
+            evento.ubicacion = request.user
+            evento.detalles_evento = request.user
+            evento.cupo = request.user
+            evento.fecha_inicio_evento = request.user
+            evento.fecha_fin_evento = request.user
+            evento.hora_inicio_evento = request.user
+            evento.hora_fin_evento = request.user
+            evento.save()
+            return redirect('Usuarios/IHPrincipal.html', pk=evento.pk)
+    else:
+        evento_form = EventoForm(instance=evento)
+    return render(request, 'Evento/IHEditarEvento.html', {'form': evento_form})
+
+def eliminar_evento(request):
+    evento = get_object_or_404(Evento, pk=pk)
+    if request.method =='POST':
+        evento_form = EventoForm(request.POST, instance=evento)
+        evento.remove()
+        return redirect('Usuarios/IHPrincipal.html', pk=evento.pk)
