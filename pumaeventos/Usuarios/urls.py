9aaf189460696217af88fa276748a6cc231f06f5
corderodedios182 <corderodedios182@gmail.com>
2019-10-20 23:50:16 -0500

    Registros

--- pumaeventos/Usuarios/urls.py
+++ pumaeventos/Usuarios/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Home, crear_Usuario
+from .views import Home, crear_Usuario, listaUsuarios, editarUsuario
 
 urlpatterns = [
-    path('',Home, name = 'IHPrincipal'),
-    path('Registrarse/',crear_Usuario, name = 'IHRegistrarse')
+    path('IHPrincipal',Home, name = 'IHPrincipal'),
+    path('Registrarse/',crear_Usuario, name = 'IHRegistrarse'),
+    path('listaUsuarios/',listaUsuarios, name = 'listaUsuarios'),
+    path('editar_usuario/<int:id_usuario>', editarUsuario, name = 'editar_usuario')
 ]
--- pumaeventos/Usuarios/views.py
+++ pumaeventos/Usuarios/views.py
@@ -1,16 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.exceptions import ObjectDoesNotExist
 from .forms import UsuarioForm
+from .models import Usuario
 
 # Create your views here.
 def Home(request):
-    return render(request, "Usuarios/IHPrincipal.html")
+    return render(request, 'Usuarios/IHPrincipal.html')
+
 
 def crear_Usuario(request):
     if request.method == 'POST':
         usuario_form = UsuarioForm(request.POST)
         if usuario_form.is_valid():
             usuario_form.save()
-            return render(request, "Usuarios/IHPrincipal.html")
+            return redirect('/IHPrincipal')
     else:
         usuario_form = UsuarioForm()
     return render(request, 'Usuarios/IHRegistrarse.html', {'usuario_form': usuario_form})
+
+
+def listaUsuarios(request):
+    Usuarios = Usuario.objects.all()
+    return render(request,'Usuarios/IHListaUsuarios.html',{'Usuarios':Usuarios})
+
+
+def editarUsuario(request, id_usuario):
+    usuario_form = None
+    error = None
+    try:
+        usuario = Usuario.objects.get(id_usuario = id_usuario)
+        if request.method == 'GET':
+            usuario_form = UsuarioForm(instance = usuario)
+        else:
+            usuario_form = UsuarioForm(request.POST, instance = usuario)
+            if usuario_form.is_valid():
+                usuario_form.save()
+            return redirect('/IHPrincipal')
+    except ObjectDoesNotExist as e:
+        error = e
+    return render(request, 'Usuarios/IHRegistrarse.html', {'usuario_form': usuario_form, 'error':error})
