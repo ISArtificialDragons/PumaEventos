35c2e5f7822dfbb6b33f63201f5f3a254b2e3b33
corderodedios182 <corderodedios182@gmail.com>
2019-11-05 11:54:41 -0600

    Urls correctas

--- pumaeventos/Eventos/urls.py
+++ pumaeventos/Eventos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 
+app_name = 'Eventos'
 urlpatterns = [
     path('CrearEvento/', CrearEvento.as_view(), name = 'IHCrearEvento'),
     path('ListarEventos/', ListarEventos.as_view(), name = 'IHListarEventos'),
